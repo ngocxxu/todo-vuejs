ddf58103ea1f52e6fde339dc52a5c926b9e9b2d9
ngocxxu <ngocquach43@gmail.com>
2023-03-12 22:11:16 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => {
+  return todos.value.sort((a, b) => b.createdAt - a.createdAt)
+})
+</script>
+
+<template>
+  Hello
+</template>
+
+
