cd03b081a9ae1ea4360bdfb0dd12c3403a560997
ngocxxu <ngocquach43@gmail.com>
2023-03-12 23:03:59 +0700

    add UI for todo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <script setup>
-import { ref, onMounted, computed, watch } from 'vue'
+import { ref, onMounted, computed, watch } from 'vue';
 
-const todos = ref([])
-const name = ref('')
+const todos = ref([]);
+const name = ref('');
 
-const input_content = ref('')
-const input_category = ref(null)
+const input_content = ref('');
+const input_category = ref(null);
 
 const todos_asc = computed(() => {
-  return todos.value.sort((a, b) => b.createdAt - a.createdAt)
-})
+  return todos.value.sort((a, b) => b.createdAt - a.createdAt);
+});
+
+const addTodo = () => {
+
+}
+
+watch(name, (newVal) => localStorage.setItem('name', newVal));
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+});
+
 </script>
 
 <template>
-  Hello
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type='text' placeholder='Name here' v-model='name' />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+
+      <form @submit.prevent='addTodo'>
+        <h4>
+          What's on your todo list
+        </h4>
+        <input type='text' placeholder='e.g. make a video' v-model='input_content' />
+
+        <h4>Pick a category</h4>
+
+        <div class="options">
+
+          <label>
+            <input type="radio" name="category" id='category' value='business' v-model='input_category' />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          
+          <label>
+            <input type="radio" name="category" id='category' value='personal' v-model='input_category' />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+
+          {{ input_category }}
+
+        </div>
+      </form>
+    </section>
+  </main>
 </template>
-
-
