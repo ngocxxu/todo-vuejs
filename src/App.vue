b6efa4d039a6c06c936861f5ea02dd3c85d48fb2
bono43 <bono43@nexondv.com>
2023-03-13 17:11:28 +0700

    complete todo vue

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,30 @@ const todos_asc = computed(() => {
 });
 
 const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) { return }
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  })
 
+  input_content.value = ''
+  input_category.value = null
 }
 
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo)
+}
+
+// Deep apply for array to check any change element/value in array
+// Ex: when change value in input tag, it'll be applied 2 way bindings
+watch(todos, (newVal) => localStorage.setItem('todos', JSON.stringify(newVal)) , { deep: true });
 watch(name, (newVal) => localStorage.setItem('name', newVal));
 
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos')) || []
 });
 
 </script>
@@ -49,17 +66,41 @@ onMounted(() => {
             <span class="bubble business"></span>
             <div>Business</div>
           </label>
-          
+
           <label>
             <input type="radio" name="category" id='category' value='personal' v-model='input_category' />
             <span class="bubble personal"></span>
             <div>Personal</div>
           </label>
 
-          {{ input_category }}
-
         </div>
+
+        <input type="submit" value="Add todo">
+
       </form>
     </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+
+        <div v-for='todo in todos_asc' :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model='todo.done'>
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model='todo.content'>
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
   </main>
 </template>
